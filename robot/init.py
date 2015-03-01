d09b227e3f721357102bd24d028245f3f1822501
KirillUdod <kirilludod@gmail.com>
2015-03-01 08:45:32 +0300

    2

--- robot/init.py
+++ robot/init.py
@@ -5,6 +5,8 @@ import sys
 from PyQt4 import QtGui, QtCore
 from err_window import Err_Window
 
+
+LINK = "http://bbpujcka.cz/"
 DICT = {
         'amount': 4000,
         'durat': 30,
@@ -16,7 +18,62 @@ DICT = {
         'ch2': True,
         }
 
-LINK = "http://bbpujcka.cz/"
 Rob(DICT, LINK)
+
+#ошибка несоответствия лимита и количества
+#DICT = {
+#        'amount': 4000,
+#        'durat': 300,
+#        'firstname': 'NAme',
+#        'lastname': 'Fam',
+#        'mob_number': '3350090',
+#        'email': 'man@i.ru',
+#        'ch1': True,
+#        'ch2': True,
+#        }
+
+#Rob(DICT, LINK)
+
+#пустое поле имени
+#DICT = {
+#        'amount': 4000,
+#        'durat': 30,
+#        'firstname': '',
+#        'lastname': 'Fam',
+#        'mob_number': '3350090',
+#        'email': 'man@i.ru',
+#        'ch1': True,
+#        'ch2': True,
+#        }
+
+#Rob(DICT, LINK)
+
+#неправильный лимит
+#DICT = {
+#        'amount': 4000,
+#        'durat': 301,
+#        'firstname': 'NAme',
+#        'lastname': 'Fam',
+#        'mob_number': '3350090',
+#        'email': 'man@i.ru',
+#        'ch1': True,
+#        'ch2': True,
+#        }
+
+#Rob(DICT, LINK)
+
+#неправильное количество
+#DICT = {
+#        'amount': 400,
+#        'durat': 30,
+#        'firstname': 'NAme',
+#        'lastname': 'Fam',
+#        'mob_number': '3350090',
+#        'email': 'man@i.ru',
+#        'ch1': True,
+#        'ch2': True,
+#        }
+
+#Rob(DICT, LINK)
 
 
